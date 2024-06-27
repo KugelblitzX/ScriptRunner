fea7e091743560f8a75ff3a4efdff1c67869b4fb
KugelblitzX <154686174+KugelblitzX@users.noreply.github.com>
2024-06-27 10:27:59 +0300

    Create test_comp_lead.groovy

---
+++ test_comp_lead.groovy
@@ -0,0 +1,71 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.user.ApplicationUser
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+
+// Логування
+def log = Logger.getLogger("com.example.jira")
+log.setLevel(Level.DEBUG)
+
+// Отримуємо поточну задачу
+Issue issue = ComponentAccessor.getIssueManager().getIssueObject(issue.key)
+
+// Встановлюємо поле звідки будемо тягнути користувача
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+def userField = customFieldManager.getCustomFieldObjectByName("JIRA user")
+def statusField = customFieldManager.getCustomFieldObjectByName("Component Lead Status")
+ApplicationUser userToCheck = issue.getCustomFieldValue(userField) as ApplicationUser
+
+log.info "Перевірка юзера ${userToCheck?.username} на статус Component Lead."
+
+if (userToCheck) {
+    boolean isLead = checkComponentLead(userToCheck, log)
+    if (isLead) {
+        log.info "Користувач ${userToCheck.username} є Component Lead."
+        setStatusField(issue, statusField, "Component Lead", log)
+    } else {
+        log.info "Користувач ${userToCheck.username} не є Component Lead."
+        setStatusField(issue, statusField, "Not Component Lead", log)
+    }
+} else {
+    log.info "Не знайшов юзера в полі 'JIRA user'"
+    setStatusField(issue, statusField, "No User", log)
+}
+
+static def checkComponentLead(ApplicationUser user, def log) {
+    // Отримуємо компоненти проектів
+    def projectComponentManager = ComponentAccessor.getProjectComponentManager()
+    def projectManager = ComponentAccessor.getProjectManager()
+    
+    // Проходимо по всіх проектах і компонентах, щоб перевірити статус Component Lead
+    def allProjects = projectManager.getProjectObjects()
+    for (project in allProjects) {
+        def components = projectComponentManager.findAllForProject(project.id)
+        for (component in components) {
+            if (component.lead == user.username) {
+                log.info "Користувач ${user.username} є лідером компонента в проекті ${project.name}"
+                return true
+            }
+        }
+    }
+    return false
+}
+
+static def setStatusField(Issue issue, def statusField, String status, def log) {
+    def issueService = ComponentAccessor.getIssueService()
+    def user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+    def issueInputParameters = issueService.newIssueInputParameters()
+    issueInputParameters.addCustomFieldValue(statusField.idAsLong, status)
+    def updateValidationResult = issueService.validateUpdate(user, issue.id, issueInputParameters)
+    if (updateValidationResult.isValid()) {
+        def updateResult = issueService.update(user, updateValidationResult)
+        if (updateResult.isValid()) {
+            log.info "Поле статусу успішно оновлено на: ${status}"
+        } else {
+            log.error "Не вдалося оновити поле статусу: ${updateResult.errorCollection}"
+        }
+    } else {
+        log.error "Не вдалося оновити поле статусу: ${updateValidationResult.errorCollection}"
+    }
+}
