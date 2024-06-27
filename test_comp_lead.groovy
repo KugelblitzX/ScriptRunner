935f791c7de745d39af29b73dbd9847626cd2fc0
KugelblitzX <154686174+KugelblitzX@users.noreply.github.com>
2024-06-27 14:14:44 +0300

    Update withoutLogs_test_comp_lead.groovy
    withoutLogs

--- test_comp_lead.groovy
+++ test_comp_lead.groovy
@@ -17,19 +17,14 @@ def userField = customFieldManager.getCustomFieldObjectByName("JIRA user")
 def statusField = customFieldManager.getCustomFieldObjectByName("Component Lead Status")
 ApplicationUser userToCheck = issue.getCustomFieldValue(userField) as ApplicationUser
 
-log.info "Перевірка юзера ${userToCheck?.username} на статус Component Lead."
-
 if (userToCheck) {
     boolean isLead = checkComponentLead(userToCheck, log)
     if (isLead) {
-        log.info "Користувач ${userToCheck.username} є Component Lead."
         setStatusField(issue, statusField, "Component Lead", log)
     } else {
-        log.info "Користувач ${userToCheck.username} не є Component Lead."
         setStatusField(issue, statusField, "Not Component Lead", log)
     }
 } else {
-    log.info "Не знайшов юзера в полі 'JIRA user'"
     setStatusField(issue, statusField, "No User", log)
 }
 
@@ -43,7 +38,6 @@ static def checkComponentLead(ApplicationUser user, def log) {
     for (project in allProjects) {
         def components = projectComponentManager.findAllForProject(project.id)
         for (component in components) {
-            log.info "Перевірка компонента ${component.name} у проекті ${project.name} для користувача ${user.username}"
             if (component.lead == user.key) {
                 log.info "Користувач ${user.username} є лідером компонента ${component.name} у проекті ${project.name}"
                 leadStatus = true
@@ -61,12 +55,5 @@ static def setStatusField(Issue issue, def statusField, String status, def log) 
     def updateValidationResult = issueService.validateUpdate(user, issue.id, issueInputParameters)
     if (updateValidationResult.isValid()) {
         def updateResult = issueService.update(user, updateValidationResult)
-        if (updateResult.isValid()) {
-            log.info "Поле статусу успішно оновлено на: ${status}"
-        } else {
-            log.error "Не вдалося оновити поле статусу: ${updateResult.errorCollection}"
-        }
-    } else {
-        log.error "Не вдалося оновити поле статусу: ${updateValidationResult.errorCollection}"
     }
 }
