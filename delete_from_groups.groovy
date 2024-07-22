4b837e88445e2e39e0b77000c159e3001ae69664
KugelblitzX <154686174+KugelblitzX@users.noreply.github.com>
2024-07-22 14:41:56 +0300

    Update delete_from_groups.groovy
    3

--- delete_from_groups.groovy
+++ delete_from_groups.groovy
@@ -7,21 +7,22 @@ def userUtil = ComponentAccessor.getUserUtil()
 
 // Получаем значение customfield_1 из текущего задания
 def customField = customFieldManager.getCustomFieldObjectByName("customfield_1")
-def userName = issue.getCustomFieldValue(customField) as String
-def user = userManager.getUserByName(userName)
+def userToRemove = issue.getCustomFieldValue(customField) as ApplicationUser
 
-if (user) {
+if (userToRemove) {
+    log.warn("User found: ${userToRemove.username}")
+
     // Получаем все группы, в которых состоит пользователь
-    def groups = groupManager.getGroupsForUser(user.name)
+    def groups = groupManager.getGroupsForUser(userToRemove.name)
     
     groups.each { group ->
         if (group) {
-            userUtil.removeUserFromGroup(group, user)
-            log.warn("User: ${user.name} removed from group: ${group.name}")
+            userUtil.removeUserFromGroup(group, userToRemove)
+            log.warn("User: ${userToRemove.username} removed from group: ${group.name}")
         } else {
             log.warn("Group does not exist")
         }
     }
 } else {
-    log.warn("User: ${userName} doesn't exist")
+    log.warn("User not found in custom field 'customfield_1'")
 }
