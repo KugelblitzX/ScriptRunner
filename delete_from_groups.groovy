1612d2bd8c03bec754a107242dc489c34610e4f8
KugelblitzX <154686174+KugelblitzX@users.noreply.github.com>
2024-07-22 14:07:59 +0300

    Update delete_from_groups.groovy
    2

--- delete_from_groups.groovy
+++ delete_from_groups.groovy
@@ -1,33 +1,27 @@
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.user.util.UserManager
-import com.atlassian.jira.bc.group.GroupService
-import com.atlassian.jira.bc.group.GroupService.RemoveUserFromGroupValidationResult
 
 def customFieldManager = ComponentAccessor.getCustomFieldManager()
 def userManager = ComponentAccessor.getUserManager()
-def groupService = ComponentAccessor.getComponent(GroupService)
+def groupManager = ComponentAccessor.getGroupManager()
+def userUtil = ComponentAccessor.getUserUtil()
 
 // Получаем значение customfield_1 из текущего задания
 def customField = customFieldManager.getCustomFieldObjectByName("customfield_1")
-def userKey = issue.getCustomFieldValue(customField) as String
-def user = userManager.getUserByKey(userKey)
+def userName = issue.getCustomFieldValue(customField) as String
+def user = userManager.getUserByName(userName)
 
 if (user) {
     // Получаем все группы, в которых состоит пользователь
-    def groups = ComponentAccessor.getGroupManager().getGroupsForUser(user.name)
+    def groups = groupManager.getGroupsForUser(user.name)
     
     groups.each { group ->
-        // Валидация удаления пользователя из группы
-        RemoveUserFromGroupValidationResult validationResult = groupService.validateRemoveUserFromGroup(user.directoryUser, group)
-        
-        if (validationResult.isValid()) {
-            // Удаление пользователя из группы
-            groupService.removeUserFromGroup(validationResult)
-            log.info("User ${user.name} removed from group ${group.name}")
+        if (group) {
+            userUtil.removeUserFromGroup(group, user)
+            log.warn("User: ${user.name} removed from group: ${group.name}")
         } else {
-            log.warn("Failed to remove user ${user.name} from group ${group.name}: ${validationResult.errorCollection}")
+            log.warn("Group does not exist")
         }
     }
 } else {
-    log.warn("User with key ${userKey} not found")
+    log.warn("User: ${userName} doesn't exist")
 }
